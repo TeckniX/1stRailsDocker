d6ced7713f896442f41ae4d8a3514ae3fa5cdd68
Teknix <Teknix@ben-mbp.home>
2015-02-11 08:45:54 -0500

    My first docker Rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Myapp::Application.config.secret_key_base = '6b72a4cb7d187e9c57e8318328eb84ce4f547e0d7ff38ca7748a832eb7e6d3c81b37557a0ca49bede52dc48b53fb70314f8a206ec36861f672b9a4e0b668ea77'
